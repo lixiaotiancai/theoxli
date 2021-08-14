79fe59402a3cd716aa09badec0400f38c6e8a447
theoxli <theoxli@tencent.com>
2021-08-14 19:55:12 +0800

    feat: 多页面打包done

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,64 @@
 const path = require('path');
 const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const devEntry = {};
 const pagesDir = path.resolve(__dirname, './src/pages/');
 
 /**
  * 获取打包入口
  */
 function getEntry() {
-  const devEntry = {};
-  const htmlPath = path.resolve(pagesDir, './**/*.js');
+
+  const htmlPath = path.resolve(pagesDir, './**/*.html');
   const htmlPathList = glob.sync(htmlPath);
 
   htmlPathList.forEach(filePath => {
-    const htmlName = filePath.match(/\/pages\/(.+)\.js/)[1];
-    devEntry[htmlName] = filePath;
+    const htmlName = filePath.match(/\/pages\/(.+)\.html/)[1];
+    devEntry[htmlName] = filePath.replace('html', 'js');
   })
 
   return devEntry;
 }
 
+/**
+ * 获取htmlwebpack插件列表
+ */
+function getHtmlWebpackPluginList() {
+  return Object.entries(devEntry).map(([chunk, entryJs]) => {
+    return new HtmlWebpackPlugin({
+      chunks: [chunk],
+      template: entryJs.replace('js', 'html'),
+      filename: `${chunk}.html`
+    })
+  })
+}
+
+/**
+ * 初始化
+ */
+function init() {
+  getEntry();
+}
+
+init();
+
 module.exports = {
   mode: 'development',
-  entry: getEntry(),
+  entry: devEntry,
   output: {
-    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist'), // 出口目录，dist文件
     filename: '[name].js',
     chunkFilename: '[name].min.js',
   },
+  plugins: [
+    ...getHtmlWebpackPluginList(),
+  ],
+  devtool: 'eval-source-map',
   devServer: {
-    publicPath: './dist',
+    publicPath: '/dist/',
     port: 8888,
     hot: true,
-  },
+  }
 }
