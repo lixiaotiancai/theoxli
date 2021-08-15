b6abf5f80596af0f09fcfb4fd6cde83ee042930b
theoxli <theoxli@tencent.com>
2021-08-15 18:16:59 +0800

    feat: 生产环境构建完毕

---
+++ src/pages/test2/index.ts
@@ -0,0 +1,12 @@
+(document.querySelector('h1') as any).style = "color: green";
+interface Asd {
+  asd: 'asd'
+}
+
+const a: Asd = {
+  asd: 'asd'
+}
+
+console.log('🔥胡哦🔥', a);
+
+export {}
---
+++ webpack.prod.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const glob = require('glob');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+const devEntry = {};
+const pagesDir = path.resolve(__dirname, './src/pages/');
+
+/**
+ * 获取打包入口
+ */
+function getEntry() {
+
+  const htmlPath = path.resolve(pagesDir, './{**,**/**}/*.html');
+  const htmlPathList = glob.sync(htmlPath);
+
+  htmlPathList.forEach(filePath => {
+    const pageName = filePath.match(/\/pages\/(.+)\.html/)[1];
+
+    ['js', 'ts', 'jsx', 'tsx']
+      .map(extension => filePath.replace('html', extension))
+      .forEach(entry => {
+        if (fs.existsSync(entry)) {
+          devEntry[pageName] = entry;
+        }
+      })
+  })
+
+  return devEntry;
+}
+
+/**
+ * 获取htmlwebpack插件列表
+ */
+function getHtmlWebpackPluginList() {
+  return Object.entries(devEntry).map(([chunk, entryJs]) => {
+    return new HtmlWebpackPlugin({
+      chunks: [chunk],
+      template: entryJs.replace(/(jsx|tsx|js|ts)/g, 'html'),
+      filename: `${chunk}.html`
+    })
+  })
+}
+
+/**
+ * 初始化
+ */
+function init() {
+  getEntry();
+}
+
+init();
+
+module.exports = {
+  mode: 'production',
+  entry: devEntry,
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 出口目录，dist文件
+    filename: '[name].[contenthash:8].js',
+    chunkFilename: '[name].[contenthash:8].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  plugins: [
+    ...getHtmlWebpackPluginList(),
+    new HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.min.js']
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "initial",
+          minChunks: 2, // 同时引用了2次才打包
+        }
+      }
+    }
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 8888,
+    hot: true,
+  }
+}
