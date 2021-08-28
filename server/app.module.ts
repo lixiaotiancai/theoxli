e724a0c6e9914d91de6dc82465f7b7839603e23c
theoxli <theoxli@tencent.com>
2021-08-28 18:19:57 +0800

    chore: nestjs启动静态资源服务器

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'pages'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
