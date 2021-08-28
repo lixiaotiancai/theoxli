567650f149699189abad1da183fbbf5689415e48
theoxli <theoxli@tencent.com>
2021-08-28 15:34:37 +0800

    feat: add nest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.eslint.json'],
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    "@typescript-eslint"
-  ],
+  root: true,
   env: {
-    "es6": true
+    node: true,
+    jest: true,
   },
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
