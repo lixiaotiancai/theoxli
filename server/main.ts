c1121999411337f863b3508a4a4fac7e073aa7b9
theoxli <theoxli@tencent.com>
2021-08-28 19:30:44 +0800

    feat: ci油画

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+/**
+ * 获取服务端启动端口
+ * 
+ * @param app 
+ * @returns 
+ */
+function getPort(app: INestApplication) {
+  return app.get(ConfigService).get('port');
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(getPort(app));
 }
 
 bootstrap();
